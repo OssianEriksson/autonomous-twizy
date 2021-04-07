71c96fbb0926129b41e133c67d26492bf2edb37e
Ossian Eriksson <OssianEriksson@users.noreply.github.com>
2021-04-07 19:29:48 +0200

    Test debugging

--- .github/test.sh
+++ .github/test.sh
@@ -13,6 +13,10 @@ cd ~/autonomous-twizy
 catkin_make
 source devel/setup.bash
 
+echo -e "\n**************************************************\n"
+rostest twizy_control test_twizy_can_control.test --text
+echo -e "\n**************************************************\n"
+
 # Run unit and integration tests
 catkin_make run_tests
 # Needed for exit code (and debugging)
