d3ff1e2b6c707d4e12d3781419d9f7eb7993959e
Ossian Eriksson <OssianEriksson@users.noreply.github.com>
2021-04-07 21:12:16 +0200

    One CI server is probably configured wrong, nmp

--- .github/test.sh
+++ .github/test.sh
@@ -13,12 +13,6 @@ cd ~/autonomous-twizy
 catkin_make
 source devel/setup.bash
 
-# Attempt to fix error "roslaunch.core.RLException: Unable to contact my own server at [...]" which appears sometimes
-ROS_HOSTNAME=localhost
-ROS_MASTER_URI="http://localhost:11311"
-roscore &
-ping `hostname` -c 1 # TODO: Clean up
-
 # Run unit and integration tests
 catkin_make run_tests
 # Needed for exit code (and debugging)
