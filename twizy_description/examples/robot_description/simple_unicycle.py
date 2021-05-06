b858fca680ae84d215be4aa90900cbe45f52fb9c
Ossian Eriksson <OssianEriksson@users.noreply.github.com>
2021-05-06 17:36:18 +0200

    Fixed unicycle example

--- twizy_description/examples/robot_description/simple_unicycle.py
+++ twizy_description/examples/robot_description/simple_unicycle.py
@@ -4,22 +4,27 @@ from twizy_description.robot_description import Node
 
 simple_unicycle = Node('Robot', {
     'children': [
-        Node('Hinge2Joint', {
-            'endPoint': Node('Solid', {
-                'physics': Node('Physics', {}),
-                'boundingObject': Node('Cylinder', {
-                    'radius': 0.2,
-                    'height': 0.05,
-                    'subdivision': 24
-                }),
-                'name': '"wheel"'
-            }),
-            'jointParameters': Node('HingeJointParameters', {
-                'axis': '0.0 0.0 1.0'
-            }),
-            'jointParameters2': Node('JointParameters', {
-                'axis': '0.0 1.0 0.0'
-            })
+        Node('Transform', {
+            'children': [
+                Node('Hinge2Joint', {
+                    'endPoint': Node('Solid', {
+                        'physics': Node('Physics', {}),
+                        'boundingObject': Node('Cylinder', {
+                            'radius': 0.2,
+                            'height': 0.05,
+                            'subdivision': 24
+                        }),
+                        'name': '"wheel"'
+                    }),
+                    'jointParameters': Node('HingeJointParameters', {
+                        'axis': '0.0 0.0 1.0'
+                    }),
+                    'jointParameters2': Node('JointParameters', {
+                        'axis': '0.0 1.0 0.0'
+                    })
+                })
+            ],
+            'translation': '0.0 0.0 -0.3'
         })
     ],
     'physics': Node('Physics', {}),
@@ -39,4 +44,3 @@ print(simple_unicycle.proto_str('SimpleUnicycle', [
 
 print('\n\n---------------- URDF ---------------------\n')
 print(simple_unicycle.urdf_str('simple_unicycle'))
-
