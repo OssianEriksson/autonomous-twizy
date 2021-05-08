b85154dba7a243d2dea5b6f420b014d084482e46
Ossian Eriksson <OssianEriksson@users.noreply.github.com>
2021-05-08 20:15:07 +0200

    Yes to install

--- install.sh
+++ install.sh
@@ -92,18 +92,18 @@ if [[ ! -z "${install_cuda}" ]]; then
 
 	wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu2004/x86_64/nvidia-machine-learning-repo-ubuntu2004_1.0.0-1_amd64.deb
 
-	sudo apt install ./nvidia-machine-learning-repo-ubuntu2004_1.0.0-1_amd64.deb
+	sudo apt install -y ./nvidia-machine-learning-repo-ubuntu2004_1.0.0-1_amd64.deb
 	sudo apt-get update
 
 	# Install NVIDIA driver
-	sudo apt-get install --no-install-recommends nvidia-driver-450
+	sudo apt-get install -y --no-install-recommends nvidia-driver-450
 	# Reboot. Check that GPUs are visible using the command: nvidia-smi
 
 	wget https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/libnvinfer7_7.1.3-1+cuda11.0_amd64.deb
-	sudo apt install ./libnvinfer7_7.1.3-1+cuda11.0_amd64.deb
+	sudo apt install -y ./libnvinfer7_7.1.3-1+cuda11.0_amd64.deb
 	sudo apt-get update
 
-	sudo apt-get install --no-install-recommends cuda-11-0 libcudnn8=8.0.5.39-1+cuda11.0 libcudnn8-dev=8.0.5.39-1+cuda11.0
+	sudo apt-get install -y --no-install-recommends cuda-11-0 libcudnn8=8.0.5.39-1+cuda11.0 libcudnn8-dev=8.0.5.39-1+cuda11.0
 fi
 
 if [[ ! -z "${TWIZY_ONBOARD}" ]]; then # If running on the Twizy's on-board computer
